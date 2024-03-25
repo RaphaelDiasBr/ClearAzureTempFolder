5810c356fecd1dcdd9eb86c3ea8ec462f3b90b17
Raphael Batista Dias <124581420+RaphaelDiasBr@users.noreply.github.com>
2024-03-24 21:38:07 -0300

    Save Windows Forms Project
    Windows Form, C#, Azure

---
+++ AzureBlobHelper.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+
+namespace AzureTempFolder
+{
+    public class AzureBlobHelper
+    {
+        private string connectionString;
+
+        public AzureBlobHelper(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public List<string> ObtainAzureInformation(string containerName, string fileName)
+        {
+            List<string> informacoesAzure = new List<string>();
+
+            try
+            {
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+                CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+                CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+                CloudBlockBlob blob = container.GetBlockBlobReference(fileName);
+
+                if (blob.Exists())
+                {
+                    informacoesAzure.Add("Sim");
+
+                    double size = blob.Properties.Length / 1024.0;
+
+                    if (size > 0)
+                        informacoesAzure.Add($"{size:N2} KB");
+                    else
+                        informacoesAzure.Add("ERROR");
+                }
+                else
+                {
+                    informacoesAzure.Add("Não");
+                    informacoesAzure.Add(string.Empty);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao obter informações no Azure: {ex.Message}");
+                informacoesAzure.Add("Erro");
+                informacoesAzure.Add(string.Empty);
+            }
+
+            return informacoesAzure;
+        }
+
+
+        public List<string> GetFilesWhitProblem(string containerName)
+        {
+            List<string> problematicFiles = new List<string>();
+
+            try
+            {
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+                CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+                CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+
+                foreach (IListBlobItem blobItem in container.ListBlobs(null, true, BlobListingDetails.None))
+                {
+                    if (blobItem is CloudBlockBlob blockBlob)
+                    {
+                        if (blockBlob.Properties.Length == 0)
+                        {
+                            problematicFiles.Add(blockBlob.Name);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao verificar no Azure: {ex.Message}");
+            }
+
+            return problematicFiles;
+        }
+    }
+}
---
+++ MenuDiagnostic.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Windows.Forms;
+
+namespace AzureTempFolder
+{
+    public partial class MenuDiagnostic : Form
+    {
+        private string subpastaSelecionada;
+        private AzureBlobHelper blobHelper;
+
+        public MenuDiagnostic(string subpastaSelecionada)
+        {
+            InitializeComponent();
+
+            this.subpastaSelecionada = subpastaSelecionada;
+
+            blobHelper = new AzureBlobHelper(ConfigurationManager.AppSettings["AzureStorageConnectionString"]);
+            SetGridView(subpastaSelecionada);
+        }
+
+        private void SetGridView(string subpasta)
+        {
+            try
+            {
+                List<string> listArquivos = blobHelper.GetFilesWhitProblem(subpasta);
+
+                DataTable dataTable = new DataTable();
+                dataTable.Columns.Add("Nome do Arquivo");
+                dataTable.Columns.Add("Tamanho do Arquivo");
+
+                if(listArquivos.Count > 0)
+                {
+                    MessageBox.Show("Foram encontrados arquivos com problemas", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    foreach (string arquivo in listArquivos)
+                    {
+                        dataTable.Rows.Add(arquivo, "0 KB");
+                    }
+
+                    dataGridView1.DataSource = dataTable;
+                    dataGridView1.AutoGenerateColumns = true;
+
+                    dataGridView1.Columns["Nome do Arquivo"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                    dataGridView1.Columns["Tamanho do Arquivo"].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                }
+                else
+                {
+                    MessageBox.Show("Não foram encontrados arquivos com problemas", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao preencher a grid: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MenuForm menuForm = new MenuForm();
+            this.Hide();
+            menuForm.ShowDialog();
+
+            this.Close();
+        }
+    }
+}
